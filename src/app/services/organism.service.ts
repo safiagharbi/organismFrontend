1ee0f4164aa212ad5fdbfa2f16992dc6ea72eec2
safiagharbi <safia.gharbi.pro@gmail.com>
2025-02-11 22:17:32 +0100

    modify conf to have different apiUrl in the dev/prod environment

--- src/app/services/organism.service.ts
+++ src/app/services/organism.service.ts
@@ -3,18 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Organism } from '../models/organism.model';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrganismService {
 
-  private apiUrl = 'http://localhost:8080/organisms';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<Organism[]> {
-    return this.http.get<Organism[]>(this.apiUrl).pipe(
+    return this.http.get<Organism[]>(`${this.apiUrl}/organisms`).pipe(
       map((data: any[]) =>
         data.map(item => ({
           id: item.id,
